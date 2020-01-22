cfa86caa6b4cdfe6c4455cfe211bb1f1f6e00721
Nicholas Sandomeno <nicholassandomeno@Nicholass-Air.home>
2020-01-22 17:04:26 -0500

    initial state taken from app and setup in reducer folder. Empty reducer function setup and imported to index.js

--- src/index.js
+++ src/index.js
@@ -5,5 +5,15 @@ import App from './App';
 import 'bulma/css/bulma.css';
 import './styles.scss';
 
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+// Will need reducer imported here and passed to createStore(**here**) as an argument
+import { carReducer } from './reducers/index.js';
+store = createStore(carReducer)
+
+
 const rootElement = document.getElementById('root');
-ReactDOM.render(<App />, rootElement);
+ReactDOM.render(
+<Provider store={store}>
+    <App />
+</Provider>, rootElement);
